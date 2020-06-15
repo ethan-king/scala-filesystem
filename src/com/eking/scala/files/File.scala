de227a73e7cef402fefea624dec613073aaa4eb6
Ethan <ethan.king@rutgers.edu>
2020-06-15 16:33:20 -0400

    implemented echo

--- src/com/eking/scala/files/File.scala
+++ src/com/eking/scala/files/File.scala
@@ -15,6 +15,12 @@ class File(override val parentPath: String, override val name: String, contents:
   override def isFile: Boolean = true
 
   override def getType: String = "File"
+
+  def setContents(newContents: String): File =
+    new File(parentPath, name, newContents)
+
+  def appendContents(newContents: String): File =
+    setContents(contents + "\n" + newContents)
 }
 
 object File {
